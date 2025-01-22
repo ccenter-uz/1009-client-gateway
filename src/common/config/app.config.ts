a7f93a54b12b4446d8df7c1af99ce3a76a3fbdc5
imron <imronshoimov125@gmail.com>
2025-01-22 17:27:46 +0500

    type change

--- src/common/config/app.config.ts
+++ src/common/config/app.config.ts
@@ -49,7 +49,7 @@ export const appConfig = registerAs(
   CONFIG_APP_TOKEN,
   (): AppConfig => ({
     host: process.env.APP_HOST || '0.0.0.0',
-    port: parseInt(process.env.APP_PORT) || 3000,
+    port: parseInt(process.env.APP_PORT) || 3001,
     cors_domains: process.env.CORS_DOMAINS || '*',
   })
 );
