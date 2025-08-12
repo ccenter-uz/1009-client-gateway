987c10577b6cdca42067dcaee74eb63d52aac7f9
Mirxonjon <mirxonjonismanov152@gmail.com>
2025-08-12 16:33:46 +0500

    fixed bug

--- src/common/config/app.config.ts
+++ src/common/config/app.config.ts
@@ -1,9 +1,10 @@
 import { Storage } from '@google-cloud/storage';
 import { registerAs } from '@nestjs/config';
 import { resolve } from 'path';
-import * as dotenv from 'dotenv';
 import * as process from 'process';
+import * as dotenv from 'dotenv';
 import { CONFIG_APP_TOKEN, CONFIG_SERVICES_RMQ_TOKEN } from 'types/config';
+dotenv.config();
 
 export const servicesRmqConfig = registerAs(
   CONFIG_SERVICES_RMQ_TOKEN,
@@ -75,6 +76,14 @@ export const JwtConfig = {
   expiresIn: process.env.EXPIRES_IN || '10d',
 };
 
+console.log(process.env.MINIO_ENDPOINT, process.env.MINIO_PORT, {
+  host: process.env.MINIO_ENDPOINT || '192.168.100.31',
+  port: +process.env.MINIO_PORT || 9000,
+  useSSL: false,
+  accessKey: process.env.MINIO_ACCESS_KEY || 'minioadmin',
+  secretKey: process.env.MINIO_SECRET_KEY || 'minioadmin',
+  bucketName: process.env.MINIO_BUCKET_NAME || '1009-admin',
+});
 
 export const MinioConfig = {
   host: process.env.MINIO_ENDPOINT || '192.168.100.31',
